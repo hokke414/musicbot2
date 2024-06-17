a45c2d7d6b66f9b5b8b254c8b88a583def9dad2d
hokke <107782524+hokke414@users.noreply.github.com>
2024-06-17 23:22:39 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -45,7 +45,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
-  name:`lavaLink GlaceYT`, 
+  name:`青春`, 
   type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
   
